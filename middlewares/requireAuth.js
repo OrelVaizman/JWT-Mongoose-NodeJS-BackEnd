b32cdb9ecc1fbe32d77b82057073df351e8f2d1e
Orel Vaizman <orelvaizman@h-MacBook-Pro-sl-Orel.local>
2021-05-27 22:39:12 +0300

    Removing unnecessary codes

--- middlewares/requireAuth.js
+++ middlewares/requireAuth.js
@@ -21,24 +21,3 @@ const requireAuth = (req, res, next) => {
 module.exports = {
 	requireAuth,
 };
-
-// const checkUser = (req, res, next) => {
-// 	const token = req.cookies.jwt;
-// 	if (token) {
-// 		jwt.verify(token, process.env.jwt_secret, async (err, decodedToken) => {
-// 			if (err) {
-// 				console.log(err.message);
-// 				res.locals.user = null;
-// 				next();
-// 			} else {
-// 				console.log(decodedToken);
-// 				let user = await User.findById(decodedToken.id);
-// 				res.locals.user = user;
-// 				next();
-// 			}
-// 		});
-// 	} else {
-// 		res.locals.user = null;
-// 		next();
-// 	}
-// };
